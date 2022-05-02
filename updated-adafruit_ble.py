1ffe05e96fb81d927d32b976eeb838238a07410c
oldblackcrow <45359956+oldblackcrow@users.noreply.github.com>
2022-05-02 16:51:10 -0400

    Create updated-adafruit_ble.py
    This is an updated version that just makes the format readable in the Clue sketch. If you use the original version, it will probably still work but may seem weird. I'm leaving the original up because the format is used in the Adafruit Bluefruit app.

---
+++ updated-adafruit_ble.py
@@ -0,0 +1,27 @@
+import time
+import board
+import busio
+import adafruit_sgp30
+
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)
+
+ble = BLERadio()
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+print("SGP30 serial #", [hex(i) for i in sgp30.serial])
+
+sgp30.iaq_init()
+sgp30.set_iaq_baseline(0x8973, 0x8AAE)
+
+while True:
+    ble.start_advertising(advertisement)
+    while not ble.connected:
+        pass
+    while ble.connected:
+        uart.write("%05d%05d" % (sgp30.eCO2, sgp30.TVOC))
+        print("**** Baseline values: eCO2 = 0x%x, TVOC = 0x%x" % (sgp30.baseline_eCO2, sgp30.baseline_TVOC))
